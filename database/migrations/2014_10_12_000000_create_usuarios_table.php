159f395a200c96dbdecf30b8f6282b167e064d98
Diego Cordeiro <sgt.chacal.d@gmail.com>
2020-08-02 01:14:01 -0300

    Implementando erros e direcionamento para tela de reservista

--- database/migrations/2014_10_12_000000_create_usuarios_table.php
+++ database/migrations/2014_10_12_000000_create_usuarios_table.php
@@ -17,42 +17,41 @@ class CreateUsuariosTable extends Migration
     public function up()
     {
         Schema::create('USUARIOS', function (Blueprint $table) {
+            //Informações Pessoais
             $table->bigIncrements('idusuario');
             $table->bigInteger('idempresa')->unsigned()->nullable();
             $table->char('usupermissao', 1);
-            //Informacoes Civis
-            $table->string('usucpf', 11);
             $table->string('name', 100);
+            $table->string('usucpf', 11);
             $table->date('usudtnascimento');
-            $table->char('usuestadoCivil', 2);
-            $table->char('ususexo', 1);
-            $table->string('usuemail',100)->unique();
-            $table->string('usutelfixo', 20);
+            $table->char('usuestadocivil', 2);
+            $table->char('usugenero', 1);
+            $table->char('usuindportdeficiente', 1);
+            $table->string('email',100)->unique();
             $table->string('usutelcelular', 20);
+            $table->string('usutelfixo', 20);
+            $table->char('usuindviagem', 1);
+            $table->char('usuindmudarcidade', 1);
             $table->char('usuindcelwhatsapp', 1);
             $table->char('usuindmsg', 1);
-            $table->char('usuindportdeficiente', 1);
+            $table->string('usuimagemurl', 512)->nullable();
             //Informacoes militares
+            $table->char('usutipoforca', 1);
             $table->char('usuindoficial', 1);
             $table->string('usucertreservista', 40);
-            $table->char('usutipoforca', 1);
             $table->string('usupostograd', 4);
             $table->string('usunomeguerra', 100);
             $table->string('usunomeultbtl', 100);            
             //Informações Sociais
-            $table->string('usuurlimagem', 512);
-            $table->string('usuurllinkedin', 512)->nullable();
-            $table->string('usuurlfacebook', 512)->nullable();
-            $table->string('usuurltwitter', 512)->nullable();
-            $table->string('usuurlyoutube', 512)->nullable();
-            $table->string('usuurlinstagram', 512)->nullable();
-            $table->string('usuurlblogsite', 512)->nullable();
-            //Informações Disponibilidade
-            $table->char('usuindviagem', 1);
-            $table->char('usuindmudarcidade', 1);
+            $table->string('usulinkedinurl', 512)->nullable();
+            $table->string('usufacebookurl', 512)->nullable();
+            $table->string('usuinstagramurl', 512)->nullable();
+            $table->string('usutwitterurl', 512)->nullable();
+            $table->string('usuyoutubeurl', 512)->nullable();
+            $table->string('usublogsiteurl', 512)->nullable();
             //Informações Segurança Usuário
             $table->timestamp('usudtverificacaoemail')->nullable();
-            $table->string('ususenha');
+            $table->string('password');
             $table->rememberToken();
             $table->char('usuindstatus', 1);
             //Informações Segurança 
