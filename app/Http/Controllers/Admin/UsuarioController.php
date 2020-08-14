c4251251b847f899259bc5a27cf401f15af8917d
Diego Cordeiro <sgt.chacal.d@gmail.com>
2020-08-13 21:31:09 -0300

    Editar Usuarios: Preenchendo campos da tela e iniciando persistência

--- app/Http/Controllers/Admin/UsuarioController.php
+++ app/Http/Controllers/Admin/UsuarioController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\User;
+
 use App\Usuarios;
 use App\Dominios\SimNao;
 use App\Http\Controllers\Controller;
@@ -37,38 +37,11 @@ class UsuarioController extends Controller{
     public function store(Request $request){
         $senhaAleatoria = UtilsController::geraSenhaAleatoria();
 
-        $indErro = false;
         $usuPermissao = $request->input('usuPermissao');
         $indStatus = 'A';
 
-        $rules = [
-            'usuPermissao'          => 'required',
-            'name'                  => 'required',
-            'usuCPF'                => 'required|unique:USUARIOS',
-            'usuDtNascimento'       => 'required',
-            'usuEstadoCivil'        => 'required',
-            'usuGenero'             => 'required',
-            'usuIndPortDeficiente'  => 'required',
-            'email'                 => 'required|unique:USUARIOS|email',
-            'usuTelCelular'         => 'required',
-            'usuTelFixo'            => 'required',
-            'usuIndViagem'          => 'required',
-            'usuIndMudarCidade'     => 'required',
-            //'usuimagemurl'          => 'required',
-            'usuTipoForca'          => 'required',
-            'usuIndOficial'         => 'required',
-            'usuCertReservista'     => 'required',
-            'usuPostoGrad'          => 'required',
-            'usuNomeGuerra'         => 'required',
-            'usuNomeUltBtl'         => 'required',
-            'usuLinkedinUrl'        => 'required'
-        ];
-
-        $messages = ['required' => 'Campo obrigatório.'];
-
-        $customAttributes = [];
-
-        $request->validate($rules, $messages, $customAttributes);
+        //Validação de Campos do formulário
+        $this->validaCampos($request, 'i');
         
         $insert = Usuarios::create([
             'usupermissao'        => $usuPermissao,
@@ -106,8 +79,8 @@ class UsuarioController extends Controller{
         
       
         if(!$insert){
-            //$request->session()->flash('alert-danger', "Erro Inesperado, verifique o log de registros.");
-            return view('admin.reservista.cadastrar', compact('activeTab'));
+            $request->session()->flash('alert-danger', "Erro Inesperado, verifique o log de registros.");
+            return view('admin.reservista.cadastrar');
         }else{
             $request->session()->flash('alert-success', 'Dados criados com sucesso.');
             
@@ -131,10 +104,101 @@ class UsuarioController extends Controller{
         return view('admin.reservista.editar')->with(compact('usuario'));
     }
     
-    public function update(){
+    public function update(Request $request){
+        $usuario = $this->usuarios->getUsuario($request->input('idUsuario'));
+        dd($request->input());
+        /*$usuPermissao = $request->input('usuPermissao');        
+
+        //Validação de Campos do formulário
+        $this->validaCampos($request,'u');
         
+        $update = Usuarios::where(['idusuario' => $request->input('idUsuario')])->update([
+            'usupermissao'        => $usuPermissao,
+            'name'                => $request->input('name'),
+            'usucpf'              => UtilsController::apenasNumeros($request->input('usuCPF')),
+            'usudtnascimento'     => Carbon::parse($request->input('usuDtNascimento'))->format('Y-m-d H:i'),
+            'usuestadocivil'      => $request->input('usuEstadoCivil'),
+            'usugenero'           => $request->input('usuGenero'),
+            'usuindportdeficiente'=> $request->input('usuIndPortDeficiente'),
+            'email'               => $request->input('email'),
+            'usutelcelular'       => UtilsController::apenasNumeros($request->input('usuTelCelular')),
+            'usutelfixo'          => UtilsController::apenasNumeros($request->input('usuTelFixo')),
+            'usuindviagem'        => $request->input('usuIndViagem'),
+            'usuindmudarcidade'   => $request->input('usuIndMudarCidade'),
+            'usuindcelwhatsapp'   => SimNao::retornaSimNaoSeVazio($request->input('usuIndCelWhatsapp')),
+            'usuindmsg'           => SimNao::retornaSimNaoSeVazio($request->input('usuIndMsg')),
+            'usuimagemurl'        => NULL,
+            'usutipoforca'        => $request->input('usuTipoForca'),
+            'usuindoficial'       => $request->input('usuIndOficial'),
+            'usucertreservista'   => $request->input('usuCertReservista'),
+            'usupostograd'        => $request->input('usuPostoGrad'),
+            'usunomeguerra'       => $request->input('usuNomeGuerra'),
+            'usunomeultbtl'       => $request->input('usuNomeUltBtl'),
+            'usulinkedinurl'      => $request->input('usuLinkedinUrl'),
+            'usufacebookurl'      => $request->input('usuFacebookUrl'),
+            'usuinstagramurl'     => $request->input('usuInstagramUrl'),
+            'usutwitterurl'       => $request->input('usuTwitterUrl'),
+            'usuyoutubeurl'       => $request->input('usuYoutubeUrl'),
+            'usublogsiteurl'      => $request->input('usuBlogSiteUrl'),
+            //'password'            => Hash::make($senhaAleatoria),
+            //'usuindstatus'        => $indStatus,
+            'usucriou'            => Auth::user()->getAuthIdentifier(),
+            'dtcadastro'          => date('Y-m-d H:i:s')
+        ]);
+        
+        if(!$update){
+            $request->session()->flash('alert-danger', "Erro Inesperado, verifique o log de registros.".$update);
+            return view('admin.reservista.editar')->with(compact('usuario'));
+        }else{
+            $request->session()->flash('alert-success', 'Dados Alterados com sucesso.');
+            
+            switch ($usuPermissao) {
+                case 'R':
+                    $rota = 'reservistas.selecionar';
+                    break;
+                case 'E':
+                    $rota = 'rep.empresa.selecionar';
+                    break;
+                case 'A':
+                    $rota = 'administrador.selecionar';
+                    break;
+            }  
+            return redirect()->route($rota, ['permissaoUsuario' => $usuPermissao,'indStatus' => $indStatus]);
+        }*/
+
     }
     
     public function destroy(){
+    }
+
+    public function validaCampos(Request $request, $tipoPersistencia){
+        $rules = [
+            'usuPermissao'          => 'required',
+            'name'                  => 'required',
+            'usuCPF'                => $tipoPersistencia == 'i' ? 'required|unique:USUARIOS' : 'required',
+            'usuDtNascimento'       => 'required',
+            'usuEstadoCivil'        => 'required',
+            'usuGenero'             => 'required',
+            'usuIndPortDeficiente'  => 'required',
+            'email'                 => $tipoPersistencia == 'i' ? 'required|unique:USUARIOS|email' : 'required',
+            'usuTelCelular'         => 'required',
+            'usuTelFixo'            => 'required',
+            'usuIndViagem'          => 'required',
+            'usuIndMudarCidade'     => 'required',
+            //'usuimagemurl'          => 'required',
+            'usuTipoForca'          => 'required',
+            'usuIndOficial'         => 'required',
+            'usuCertReservista'     => 'required',
+            'usuPostoGrad'          => 'required',
+            'usuNomeGuerra'         => 'required',
+            'usuNomeUltBtl'         => 'required',
+            'usuLinkedinUrl'        => 'required'
+        ];
+
+        $messages = ['required' => 'Campo obrigatório.'];
+
+        $customAttributes = [];
+
+        $request->validate($rules, $messages, $customAttributes);
     }
 }
