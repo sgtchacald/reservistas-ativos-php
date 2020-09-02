02cc6c77de18eda0f5bb66639889e52049133eae
Diego Cordeiro <sgt.chacal.d@gmail.com>
2020-09-02 16:20:14 -0300

    Geral: Adicionando botão voltar nos formularios

--- resources/views/admin/nivelEstudo/editar.blade.php
+++ resources/views/admin/nivelEstudo/editar.blade.php
@@ -84,7 +84,8 @@
 				</div>
         	
         	<div class="card-footer">
-              <button type="submit" class="btn btn-primary">Salvar</button>
+			  <button type="submit" class="btn btn-primary">Salvar</button>
+			  <a href="{{ url()->previous() }}" class="btn btn-secondary">Voltar</a>
             </div>
         </div>
 	</form>		
